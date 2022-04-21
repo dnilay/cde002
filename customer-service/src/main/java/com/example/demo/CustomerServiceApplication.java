052d70b2c487cc174df69489ba9e5a9f65f46cd8
Nilay <54245048+dnilay@users.noreply.github.com>
2022-04-21 14:57:49 +0530

    Add files via upload

--- customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -1,15 +1,34 @@
 package com.example.demo;
 
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import com.example.demo.model.Customer;
+import com.example.demo.repo.CustomerRepository;
+
+import lombok.AllArgsConstructor;
+
 @SpringBootApplication
 @EnableEurekaClient
-public class CustomerServiceApplication {
+@AllArgsConstructor
+public class CustomerServiceApplication implements CommandLineRunner {
+	private final CustomerRepository customerRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		customerRepository.save(new Customer(1, UUID.randomUUID().toString(), "John", "Hyd", "john@email.com"));
+		customerRepository.save(new Customer(2, UUID.randomUUID().toString(), "Marry", "Banglore", "marry@email.com"));
+		customerRepository.save(new Customer(3, UUID.randomUUID().toString(), "Sachin", "Mum", "sachin@email.com"));
+		customerRepository.save(new Customer(4, UUID.randomUUID().toString(), "Rahul", "Banglore", "rahul@email.com"));
+
 	}
 
 }
