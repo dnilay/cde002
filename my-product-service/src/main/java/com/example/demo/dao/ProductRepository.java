8bd0240f39fa32428328f423e6dd1ae17bf66f2a
Nilay <54245048+dnilay@users.noreply.github.com>
2022-04-26 14:58:43 +0530

    Add files via upload

--- my-product-service/src/main/java/com/example/demo/dao/ProductRepository.java
+++ my-product-service/src/main/java/com/example/demo/dao/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Product;
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+	
+	@Query
+	List<Product> findByProductName(String productName);
 
 }
